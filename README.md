0c13824d5aa2cb86bb3d063d9ae6993621007bbc
prakashyari <prakashyari918@gmail.com>
2026-02-20 11:59:17 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Smart Greeting App
+
+This is a beginner Python project.
+
+## Features
+- Takes user name
+- Takes user age
+- Calculates approximate birth year
+- Prints personalized message
+
+## How to Run
---
+++ main.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+
+current_year = datetime.now().year
+birth_year = current_year - age
+
+print("\nHello,", name + "!")
+print("You were born in approximately", birth_year)
+print("Welcome to your AI journey 🚀")
